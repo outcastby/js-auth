7f6c1577226c23a235c6991c7f04000b5dece9f2
Mikhail Kruglik <miheykrug@mail.ru>
2020-02-26 17:35:42 +0300

    Fix handleTokenError

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import { Cookies, Config, Action, Dictionary } from '@outcast.by/js-ext'
+import { Cookies, Config, GQLAction, Dictionary, GQL } from '@outcast.by/js-ext'
 import { refreshToken } from '../redux/actions'
 
 const logout = (): void => {
@@ -11,19 +11,18 @@ const logout = (): void => {
 const MAX_ERROR_COUNT = 5
 let refreshTokenErrorCount = 0
 
-const handleTokenError = (action: Action): any => {
+const handleTokenError = (action: GQLAction): Promise<any> => {
   const dispatch = Config.get(['jsAuth', 'dispatch'])
 
-  dispatch(refreshToken({ refreshToken: Cookies.getDecrypted(Config.get(['jsAuth', 'refreshTokenKey'])) }))
-    .then((resp: object) => {
-      dispatch(action)
-      return resp
+  return dispatch(refreshToken({ refreshToken: Cookies.getDecrypted(Config.get(['jsAuth', 'refreshTokenKey'])) }))
+    .then(() => {
+      return GQL.Fetch.run(action.request.query, action.request.variables)
     })
     .catch((errors: Dictionary<any>) => {
       if (_.some(errors.message, ['message', 'invalid_refresh_token'])) {
         if (refreshTokenErrorCount < MAX_ERROR_COUNT) {
           refreshTokenErrorCount++
-          dispatch(action)
+          return GQL.Fetch.run(action.request.query, action.request.variables)
         } else {
           logout()
         }
