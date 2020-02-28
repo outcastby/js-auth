d6bdd73eb80ecff6ab5515bc6f4cd39df89b7309
Mikhail Kruglik <miheykrug@mail.ru>
2020-02-28 16:20:48 +0300

    Fix logic of invalid tokens errors handlers

---
+++ src/initializers/axios.ts
@@ -0,0 +1,75 @@
+import { Cookies, Config, Dictionary } from '@outcast.by/js-ext'
+import { refreshToken } from '../redux/actions'
+import _ from 'lodash'
+import { AxiosStatic } from 'axios'
+import Auth from '../utils/auth'
+
+interface State {
+  isRefreshing: boolean
+  subscribers: Function[]
+}
+
+const authAxiosInitializer = (axios: AxiosStatic): void => {
+  let state: State = {
+    isRefreshing: false,
+    subscribers: [],
+  }
+
+  const subscribeTokenRefresh = (cb: Function): void => {
+    state = { ...state, subscribers: [...state.subscribers, cb] }
+  }
+
+  const onRrefreshed = (token: string): void => {
+    state.subscribers.map((cb) => cb(token))
+  }
+
+  axios.interceptors.request.use(
+    (request) => {
+      if (Cookies.get(Config.get(['jsAuth', 'accessTokenKey']))) {
+        request.headers.common['Authorization'] = Cookies.getDecrypted('accessToken')
+      }
+      return request
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+  axios.interceptors.response.use(undefined, (err) => {
+    const {
+      config: originalRequest,
+      response: { data },
+    } = err
+
+    if (data === 'invalid_access_token') {
+      const token = Cookies.getDecrypted(Config.get(['jsAuth', 'refreshTokenKey']))
+      if (!state.isRefreshing) {
+        state = { ...state, isRefreshing: true }
+
+        const dispatch = Config.get(['jsAuth', 'dispatch'])
+
+        dispatch(refreshToken({ refreshToken: token }))
+          .then((response: Dictionary<any>) => {
+            const { accessToken } = response
+            state.isRefreshing = false
+            onRrefreshed(accessToken)
+            state = { ...state, subscribers: [] }
+          })
+          .catch((errors: Dictionary<any>) => {
+            if (_.some(errors.message, ['message', 'invalid_refresh_token'])) {
+              Auth.logout()
+            }
+          })
+      }
+      const requestSubscribers = new Promise((resolve) => {
+        subscribeTokenRefresh((token: string) => {
+          originalRequest.headers.Authorization = token
+          resolve(axios(originalRequest))
+        })
+      })
+      return requestSubscribers
+    }
+    return Promise.reject(err)
+  })
+}
+
+export default authAxiosInitializer
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,4 @@
-import _ from 'lodash'
-import { Cookies, Config, GQLAction, Dictionary, GQL } from '@outcast.by/js-ext'
-import { refreshToken } from '../redux/actions'
+import { Cookies, Config } from '@outcast.by/js-ext'
 
 const logout = (): void => {
   Cookies.remove(Config.get(['jsAuth', 'accessTokenKey']))
@@ -8,29 +6,6 @@ const logout = (): void => {
   window.location.replace('/auth')
 }
 
-const MAX_ERROR_COUNT = 5
-let refreshTokenErrorCount = 0
-
-const handleTokenError = (action: GQLAction): Promise<any> => {
-  const dispatch = Config.get(['jsAuth', 'dispatch'])
-
-  return dispatch(refreshToken({ refreshToken: Cookies.getDecrypted(Config.get(['jsAuth', 'refreshTokenKey'])) }))
-    .then(() => {
-      return GQL.Fetch.run(action.request.query, action.request.variables)
-    })
-    .catch((errors: Dictionary<any>) => {
-      if (_.some(errors.message, ['message', 'invalid_refresh_token'])) {
-        if (refreshTokenErrorCount < MAX_ERROR_COUNT) {
-          refreshTokenErrorCount++
-          return GQL.Fetch.run(action.request.query, action.request.variables)
-        } else {
-          logout()
-        }
-      }
-    })
-}
-
 export default {
   logout,
-  handleTokenError,
 }
